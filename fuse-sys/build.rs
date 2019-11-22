1cd34dbb552c7feedb03421bc578b75468985b61
Robin Ole Heinemann <robin.ole.heinemann@t-online.de>
2019-11-22 16:35:28 +0100

    add static cross compile hack

--- fuse-sys/build.rs
+++ fuse-sys/build.rs
@@ -4,10 +4,25 @@ const LIBFUSE_NAME: &str = "fuse";
 #[cfg(target_os = "macos")]
 const LIBFUSE_NAME: &str = "osxfuse";
 
+use std::env;
+
 fn main() {
-    pkg_config::Config::new()
-        .atleast_version("2.6.0")
-        .probe(LIBFUSE_NAME)
-        .map_err(|e| eprintln!("{}", e))
-        .unwrap();
+    match env::var("FUSE_CROSS_STATIC_LIB") {
+        Ok(val) => {
+            println!("cargo:rustc-link-lib=static={}", val);
+            match env::var("FUSE_CROSS_STATIC_PATH") {
+                Ok(val) => {
+                    println!("cargo:rustc-link-search=native={}", val);
+                },
+                Err(_) => {}
+            }
+        },
+        Err(_) => {
+            pkg_config::Config::new()
+                .atleast_version("2.6.0")
+                .probe(LIBFUSE_NAME)
+                .map_err(|e| eprintln!("{}", e))
+                .unwrap();
+        }
+    }
 }
